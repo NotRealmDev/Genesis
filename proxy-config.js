29ca0a4358855a35b192afd5d273d2e4fca01b59
RealmGames <104637550+NotRealmDev@users.noreply.github.com>
2026-09-12 17:15:13 -0700

    Changes

--- proxy-config.js
+++ proxy-config.js
@@ -1,13 +1,6 @@
 /*
  Genesis Proxy configuration.
-
- After you deploy genesis-proxy-worker.js to Cloudflare Workers,
- paste the Worker URL below, for example:
-
- endpoint: "https://genesis-proxy.YOURNAME.workers.dev"
-
- Leave NO trailing slash.
 */
 window.GENESIS_PROXY = {
-  endpoint: "PASTE_YOUR_WORKER_URL_HERE"
+  endpoint: "https://genesis-proxy.bernye-eugene7.workers.dev"
 };
